29732c13c11a9b6d5c070850465ee4783b9bf8ca
Mino Riestra Artime <belarmino.artime@elision.eu>
2023-03-01 17:49:15 +0100

    Command Line Runner

---
+++ src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -0,0 +1,44 @@
+package guru.springfamework.bootstrap;
+
+import guru.springfamework.domain.Category;
+import guru.springfamework.repositories.CategoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+//CommandLineRunner.run is executed on server start.
+//args can contain variables passed when the project is started
+@Component
+public class Bootstrap implements CommandLineRunner {
+
+    private CategoryRepository categoryRepository;
+
+    public Bootstrap(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Category fruits = new Category();
+        fruits.setName("Fruits");
+
+        Category dried = new Category();
+        fruits.setName("Dried");
+
+        Category fresh = new Category();
+        fruits.setName("Fresh");
+
+        Category exotic = new Category();
+        fruits.setName("Exotic");
+
+        Category nuts = new Category();
+        fruits.setName("Nuts");
+
+        categoryRepository.save(fruits);
+        categoryRepository.save(dried);
+        categoryRepository.save(fresh);
+        categoryRepository.save(exotic);
+        categoryRepository.save(nuts);
+
+        System.out.println("Data loaded = " + categoryRepository.count());
+    }
+}
